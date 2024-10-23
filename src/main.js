c52da968ce4a3d7cadbd441fe9145fcd6ab76642
zenghao <1054092535@qq.com>
2024-10-23 21:18:23 +0800

    feat✨: 项目初始化、配置样式变量

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './assets/main.css'
+import './styles/index.scss'
 
-import { createApp } from 'vue'
+import {
+  createApp
+} from 'vue'
 import App from './App.vue'
 import router from './router'
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,56 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {
+  createRouter,
+  createWebHistory
+} from 'vue-router'
+import IconView from '../views/IconView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
+  history: createWebHistory(
+    import.meta.env.BASE_URL),
+  // 首页是直接加载的，其余组件是按需加载的
+  routes: [{
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'icon',
+      component: IconView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/button',
+      name: 'button',
+      component: () => import('../views/ButtonView.vue')
+    },
+    {
+      path: '/card',
+      name: 'card',
+      component: () => import('../views/CardView.vue')
+    },
+    {
+      path: '/dialog',
+      name: 'dialog',
+      component: () => import('../views/DialogView.vue')
+    },
+    {
+      path: '/collapse',
+      name: 'collapse',
+      component: () => import('../views/CollapseView.vue')
+    },
+    {
+      path: '/pager',
+      name: 'pager',
+      component: () => import('../views/PagerView.vue')
+    },
+    {
+      path: '/tooltip',
+      name: 'tooltip',
+      component: () => import('../views/TooltipView.vue')
+    },
+    {
+      path: '/dropdown',
+      name: 'dropdown',
+      component: () => import('../views/DropdownView.vue')
     }
-  ]
+  ],
+  // 针对router-link的激活样式设置
+  linkActiveClass: 'active'
 })
 
 export default router
