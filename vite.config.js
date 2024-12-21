36513d3e32fa702037d98f3a838b05ae39dff789
zenghao <1054092535@qq.com>
2024-12-21 22:00:19 +0800

    feat✨: github pages部署

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+import {
+  fileURLToPath,
+  URL
+} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ZH-plus2/',
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src',
+        import.meta.url))
     }
   }
 })
